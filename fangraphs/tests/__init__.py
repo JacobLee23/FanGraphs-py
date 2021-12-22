b09cea881b26a784a4d01377747e67264c5646b0
Jacob Lee <JLpython@outlook.com>
2021-12-22 11:56:23 -0800

    Update unit tests for fangraphs.leaders

--- fangraphs/tests/__init__.py
+++ fangraphs/tests/__init__.py
@@ -9,40 +9,81 @@ import os
 import json
 from urllib.request import urlopen
 
+import bs4
+from playwright.sync_api import sync_playwright
+
 
 QTYPES = (
     "selections", "dropdowns", "checkboxes", "switches"
 )
 
 
-def _test_address(address: str) -> None:
+def get_soup(address: str) -> bs4.BeautifulSoup:
     """
 
     :param address:
+    :return:
     """
-    assert urlopen(address).code == 200
+    with sync_playwright() as play:
+        browser = play.chromium.launch()
+        page = browser.new_page()
+        page.goto(address, timeout=0)
+        soup = bs4.BeautifulSoup(page.content(), features="lxml")
+        browser.close()
+    return soup
 
 
-def _test_path(path: str) -> None:
+class BaseTests:
     """
 
-    :param path:
     """
-    assert os.path.exists(path)
+    @staticmethod
+    def _test_address(address: str) -> None:
+        """
 
+        :param address:
+        """
+        assert urlopen(address).code == 200
 
-def _test_file_contents(path: str) -> None:
-    """
+    @staticmethod
+    def _test_path(path: str) -> None:
+        """
 
-    :param path:
-    """
-    with open(path, "r", encoding="utf-8") as file:
-        data = json.load(file)
-    assert data
+        :param path:
+        """
+        assert os.path.exists(path)
 
-    assert isinstance(data, dict)
-    assert all(k in QTYPES for k in data)
-    assert all(isinstance(v, dict) for v in data.values())
-    assert all(isinstance(v, dict) for k in data for v in data[k].values())
-    assert all(isinstance(v, (str, list)) for k in data for k_ in data[k] for v in data[k][k_].values())
-    assert all(isinstance(e, str) for k in data for k_ in data[k] for v in data[k][k_] for e in v if isinstance(v, list))
+    @staticmethod
+    def _test_file_contents(path: str) -> None:
+        """
+
+        :param path:
+        """
+        with open(path, "r", encoding="utf-8") as file:
+            data = json.load(file)
+        assert data
+
+        assert isinstance(data, dict)
+        assert all(k in QTYPES for k in data)
+        assert all(isinstance(v, dict) for v in data.values())
+        assert all(isinstance(v, dict) for k in data for v in data[k].values())
+        assert all(isinstance(v, (str, list)) for k in data for k_ in data[k] for v in data[k][k_].values())
+        assert all(isinstance(e, str) for k in data for k_ in data[k] for v in data[k][k_] for e in v if isinstance(v, list))
+
+    @staticmethod
+    def _test_selector(soup: bs4.BeautifulSoup, css: str) -> None:
+        """
+
+        :param soup:
+        :param css:
+        """
+        assert len(soup.select(css)) == 1
+
+    @staticmethod
+    def _test_selectors(soup: bs4.BeautifulSoup, css: list[str]) -> None:
+        """
+
+        :param soup:
+        :param css:
+        """
+        assert all(len(soup.select(c)) == 1 for c in css)
--- fangraphs/tests/test_leaders.py
+++ fangraphs/tests/test_leaders.py
@@ -9,45 +9,63 @@ import inspect
 
 import pytest
 
-from . import _test_address
-from . import _test_file_contents
-from . import _test_path
+from . import BaseTests
+from . import get_soup
 from .. import leaders
 
 
-classes = tuple(
+CLASSES = tuple(
     cls for name, cls in inspect.getmembers(leaders, inspect.isclass) if name != "FanGraphsPage"
 )
 
 
 @pytest.mark.parametrize(
-    "cls", classes
+    "cls", CLASSES
 )
 def test_address(cls) -> None:
     """
 
     :param cls:
     """
-    _test_address(cls.address)
+    BaseTests._test_address(cls.address)
 
 
 @pytest.mark.parametrize(
-    "cls", classes
+    "cls", CLASSES
 )
 def test_path(cls) -> None:
     """
 
     :param cls:
     """
-    _test_path(cls.path)
+    BaseTests._test_path(cls.path)
 
 
 @pytest.mark.parametrize(
-    "cls", classes
+    "cls", CLASSES
 )
 def test_file_contents(cls) -> None:
     """
 
     :param cls:
     """
-    _test_file_contents(cls.path)
+    BaseTests._test_file_contents(cls.path)
+
+
+@pytest.mark.parametrize(
+    "cls", CLASSES
+)
+def test_filter_queries(cls) -> None:
+    """
+
+    :param cls:
+    """
+    soup = get_soup(cls.address)
+
+    for qtypedict in cls.filter_queries.values():
+        for querydict in qtypedict.values():
+            for css in querydict.values():
+                if isinstance(css, str):
+                    BaseTests._test_selector(soup, css)
+                else:
+                    BaseTests._test_selectors(soup, css)
